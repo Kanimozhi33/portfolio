1676bc9a2bfe097fda4b21e63d612d3440b06230
kanimozhi33 <kanimozhi02vm@gmail.com>
2025-05-22 23:15:17 +0530

    1

--- vite-project/src/Projects.jsx
+++ vite-project/src/Projects.jsx
@@ -4,14 +4,19 @@ import { BsGithub } from "react-icons/bs";
 const Projects = ()=>{
     return (
         <div class="whole-div bg-black">
-            <div className="img bg-white h-[300px]">
+            <div className="img bg-white h-[300px] ">
                         img
             </div>
-            <div className="text-gray-600 mt-10">
-                <h1 className="font-oswald text-9xl">01</h1>
-                <h2 className="text-teal-400">MEAL MINIT</h2>
-                <h2 className="">This is an Online Food Ordering application</h2>
-                <p>React, Javascript, Tailwind Css, Redux</p>
+            <div className="text-gray-600 mt-10 flex">
+                <div>
+                    <h1 className="font-oswald text-9xl p-5">01</h1>
+                </div>
+                <div className="ml-10 justify-center items-center p-10">
+                    <h2 className="text-teal-400 text-2xl font-bebas">MEAL MINIT</h2>
+                    <h2 className=" text-xl">This is an Online Food Ordering application</h2>
+                    <p>React, Javascript, Tailwind Css, Redux</p>
+                </div>
+                
 
             </div>
             <div className="text-gray-400">
