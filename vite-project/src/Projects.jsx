88dacfbe7375f58384533bb0b339e521fd9f745d
kanimozhi33 <kanimozhi02vm@gmail.com>
2025-05-26 23:26:53 +0530

    scdvhb

--- vite-project/src/Projects.jsx
+++ vite-project/src/Projects.jsx
@@ -4,7 +4,7 @@ import { BsGithub } from "react-icons/bs";
 const Projects = ()=>{
     return (
         <div class="whole-div bg-black">
-            <div className="img bg-white h-[300px] ">
+            <div className="img bg-white h-[500px] mt-10 w-[800px] justify-center items-center mx-auto">
                         img
             </div>
             <div className="text-gray-600 mt-10 flex">
@@ -19,9 +19,9 @@ const Projects = ()=>{
                 
 
             </div>
-            <div className="text-gray-400">
-                <hr className="text-gray-500"></hr>
-                <div className="flex"><FiExternalLink />
+            <div className="text-gray-400 ">
+                <hr className="text-gray-500 opacity-50 text-4xl"></hr>
+                <div className="flex  text-3xl space-x-4 p-8"><FiExternalLink />
                 <BsGithub /></div>
             </div>
 
